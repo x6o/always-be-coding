d9e7b96c83ac93be0469b9c35e3d41439084e3a5
Andrei Talpa <andrei.talpa@cgi.com>
2020-12-21 08:45:58 +0000

    Sumbission and onChange on controlled inputs

--- courses/react/react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ courses/react/react-advanced-2020/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -7,9 +7,12 @@ import React, { useState } from 'react';
 
 const ControlledInputs = () => {
 
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello world');
+    console.log(firstName, email);
   };
 
   return <>
@@ -17,11 +20,23 @@ const ControlledInputs = () => {
       <form className="form" onSubmit={handleSubmit}>
         <div className="form-control">
           <label style={{"color": "black"}} htmlFor="firstName">Name: </label>
-          <input type="text" id="firstName" name="firstName"/>
+          <input 
+            type="text" 
+            id="firstName" 
+            name="firstName" 
+            value={firstName} 
+            onChange={(e)=> setFirstName(e.target.value)}
+          />
         </div>
         <div className="form-control">
           <label style={{"color": "black"}} htmlFor="email">E-mail: </label>
-          <input type="text" id="email" name="email"/>
+          <input 
+            type="text" 
+            id="email" 
+            name="email" 
+            value={email} 
+            onChange={(e)=> setEmail(e.target.value)}
+          />
         </div>
         <button type="submit">Add person</button>
       </form>
