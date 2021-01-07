94f6d77b75b2ab2a4c70938468d38888fc00d4a0
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-07 08:59:58 +0000

    React: Accordion mini-project

--- courses/react/react-projects/4-accordion/setup/src/App.js
+++ courses/react/react-projects/4-accordion/setup/src/App.js
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  return <h2>accordion project setup</h2>;
+  return <main>
+    <div className="container">
+      <h3>Questions And Answers About Login</h3>
+      <section className="info">
+        {
+          data.map((question) => {
+            return (
+              <SingleQuestion key={question.id} {...question} />
+            )
+          })
+        }
+      </section>
+      
+    </div>
+  </main>;
 }
 
 export default App;
