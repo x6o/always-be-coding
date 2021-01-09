03f6b43c8708248e690ff18b71e89750e9ec5515
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-09 18:51:45 +0000

    React: Menu item mini-project

--- courses/react/react-projects/5-menu/setup/src/Categories.js
+++ courses/react/react-projects/5-menu/setup/src/Categories.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({categories, filterItems, searchItem}) => {
+  const [searchText, setSearchText] = useState("");
+
+  const textSearch = (query) => {
+      setSearchText(query);
+      if(query){
+        searchItem(query);
+      } else {
+        filterItems("all");
+      }
+  };
+
+  return <>
+    <div className="btn-container">
+      {categories.map((category, index) => {
+        return <button type="button" className="filter-btn" key={index} onClick={() => filterItems(category)}>
+          {category}
+        </button>
+      })}
+      <div className="searchbox">
+        <input type="text" value={searchText} onChange={(e) => textSearch(e.target.value)} placeholder="Search item..."></input>
+      </div>
+    </div>
+  </>;
 };
 
 export default Categories;
--- courses/react/react-projects/5-menu/setup/src/Menu.js
+++ courses/react/react-projects/5-menu/setup/src/Menu.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const boldString = (string, substr) => string.replace(RegExp(substr, 'g'), `<b style="background-color: yellow;">${substr}</b>`);
+
+const Menu = ({items, searchText}) => {
+  return <div className="section-center">
+    {
+    items.length > 0 ? items.map((menuItem) => {
+      const {id, title, img, desc, price} = menuItem;
+      let formattedTitle = "";
+      if (searchText) {
+        const index = title.indexOf(searchText);
+        formattedTitle = boldString(title, searchText);
+      }
+      return <article key={id} className="menu-item">
+          <img src={img} alt={title} className="photo"/>
+          <div className="item-info">
+            <header>
+              <div dangerouslySetInnerHTML={{__html: formattedTitle || title}}></div>
+              <h4 className="price">{price}</h4>
+            </header>
+            <p className="item-text">{desc}</p>
+          </div>
+      </article>
+    }) : "No results..."}
+  </div>
 };
 
 export default Menu;
