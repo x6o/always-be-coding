ac6287e4cd5a07e4c0a8059d8ac53747b89a8757
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-13 22:54:00 +0000

    React: Color generator mini-project [2/2]

--- courses/react/react-projects/9-color-generator/setup/src/App.js
+++ courses/react/react-projects/9-color-generator/setup/src/App.js
@@ -6,22 +6,35 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState(""); 
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values('#f15025').all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello from handleSubmit");
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
   }
   return <>
     <section className="container">
       <h3>Color generator</h3>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="#f15025" value={color} onChange={(e)=> setColor(e.target.value)} />
+        <input type="text"
+         placeholder="#f15025" value={color} 
+         onChange={(e)=> setColor(e.target.value)} 
+         className={`${error ? 'error' : null}`} />
         <button className="btn" type="submit">Submit</button>
       </form>
     </section>
     <section className="colors">
-      <h4>list goes here</h4>
+         {
+           list.map((color, index) =>{
+             return <SingleColor key={index} {...color} index={index} />
+           })
+         }
     </section>
   </>
 }
