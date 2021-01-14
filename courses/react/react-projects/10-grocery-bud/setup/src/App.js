bba675af3add9eb68498382380c264c10db0179c
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-14 21:58:44 +0000

    React: Grocery buddy mini-project: Basic Structure and add functionality

--- courses/react/react-projects/10-grocery-bud/setup/src/App.js
+++ courses/react/react-projects/10-grocery-bud/setup/src/App.js
@@ -3,7 +3,58 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({show:false, msg:'', type:''});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // display alert
+    }
+    else if (name && isEditing)
+    {
+      // deal with edit
+    }
+    else
+    {
+      // show alert
+      const newItem = {
+        id: new Date().getTime().toString(),
+        title: name,
+      }
+      setList([...list, newItem]);
+      setName('');
+    }
+  }
+
+  return <section className="section-center">
+    <form className="grocery-form" onSubmit={handleSubmit}>
+      {alert.show && <Alert />}
+      <h3>grocery buddy</h3>
+      <div className="form-control">
+        <input type="text" 
+        className="grocery" 
+        placeholder="e.g. eggs"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        />
+        <button type="submit" className="submit-btn">
+          {isEditing ? 'edit' : 'submit'}
+        </button>
+      </div>
+    </form>
+    {
+      list.length > 0 && (
+        <div className="grocery-container">
+          <List items={list} />
+          <button className="clear-btn" >clear items</button>
+        </div>
+      )
+    }
+  </section>
 }
 
 export default App
