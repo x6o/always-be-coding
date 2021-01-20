84512f94ab3a73b57809c8c3935360d8395a3989
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-20 23:37:13 +0000

    React: Cocktail app mini-project: Navbar, About and Error pages

--- courses/react/react-projects/15-cocktails/setup/src/pages/About.js
+++ courses/react/react-projects/15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+<section className="section about-section">
+  <h1 className="section-title">about us</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis aperiam totam incidunt natus dolorem dolores, ex ipsa autem laboriosam? Commodi sunt autem odio repellat explicabo. Expedita quae eligendi quidem sequi?
+    Eaque cupiditate neque nam blanditiis! Necessitatibus atque, architecto id doloribus fugiat, aperiam unde a alias culpa itaque explicabo impedit! Magnam veritatis quaerat doloremque ducimus nobis soluta debitis ex impedit voluptatibus!
+    Praesentium fugit quia minus consectetur eaque modi sapiente dolorum voluptatibus deserunt odio maiores animi repudiandae corrupti accusamus reprehenderit necessitatibus delectus, provident quasi et asperiores voluptate numquam vel obcaecati similique. Voluptate?
+    Deserunt eligendi, cumque nemo accusamus quas vitae asperiores. Optio corrupti numquam nam quod cum, aperiam voluptas delectus, recusandae blanditiis sunt voluptate suscipit vero error repellendus ipsa quam rem saepe deleniti!
+  </p>
+</section>
   )
 }
 
