8b8f60a948a21768ef76176c89428dbe9b6a6139
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-20 23:48:00 +0000

    React: Cocktail app mini-project: Added Context

--- courses/react/react-projects/15-cocktails/setup/src/components/CocktailList.js
+++ courses/react/react-projects/15-cocktails/setup/src/components/CocktailList.js
@@ -4,6 +4,18 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+
+  if(loading){
+    return <Loading />;
+  }
+
+  if(cocktails.length < 1){
+    return (<h2 className="section-title">
+      no cocktails found
+    </h2>);
+  }
+  
   return (
     <div>
       <h2>cocktail list component</h2>
