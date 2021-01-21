a6eb978015050ced1bede74e2b40fcbff5fc83ea
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-21 08:43:48 +0000

    React: Cocktail app mini-project: Add cocktail page

--- courses/react/react-projects/15-cocktails/setup/src/context.js
+++ courses/react/react-projects/15-cocktails/setup/src/context.js
@@ -9,7 +9,7 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('ma');
   const [cocktails, setCocktails] = useState([]);
 
-  const fetchDrinks = async () => {
+  const fetchDrinks = useCallback(async () => {
     setLoading(true);
 
     try {
@@ -44,12 +44,11 @@ const AppProvider = ({ children }) => {
       console.log(error);
       setLoading(false);
     }
-  }
-
+  }, [searchTerm]);
 
   useEffect(()=>{
     fetchDrinks();
-  }, [searchTerm]);
+  }, [searchTerm, fetchDrinks]);
 
   return <AppContext.Provider
    value={{
