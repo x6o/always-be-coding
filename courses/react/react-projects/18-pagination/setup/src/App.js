e1ab72e6ce30960a1ca91d4de1ae5727e9de6e87
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-26 18:09:50 +0000

    React: Pagination project [2/2]

--- courses/react/react-projects/18-pagination/setup/src/App.js
+++ courses/react/react-projects/18-pagination/setup/src/App.js
@@ -8,8 +8,32 @@ function App() {
 
   useEffect(() => {
     if(loading) return
-    setFollowers(data[page])
-  }, [loading]);
+    setFollowers(data[page]);
+  }, [loading,page]);
+
+  const handlePage = (index) => {
+    setPage(index);
+  }
+
+  const prevPage = () => {
+    setPage((currentPage) => {
+      let prevPage = currentPage - 1;
+      if (prevPage < 0) {
+        prevPage = data.length -1 ;
+      }
+      return prevPage;
+    })
+  }
+
+  const nextPage = () => {
+    setPage((currentPage) => {
+      let nextPage = currentPage + 1;
+      if (nextPage > data.length - 1) {
+        nextPage = 0;
+      }
+      return nextPage;
+    })
+  }
 
   return <main>
     <div className="section-title">
@@ -18,11 +42,31 @@ function App() {
     <section className="followers">
       <div className="container">
         {
-          data.map((follower) => {
+          followers.map((follower) => {
             return <Follower key={follower.id} {...follower} />
           })
         }
       </div>
+      {
+        !loading && (<div className="btn-container">
+          <buttton className="prev-btn" onClick={prevPage}>
+            Prev
+          </buttton>
+          {
+            data.map((item, index) => {
+              return <button key={index} 
+              className={`page-btn ${index === page ? 'active-btn' : null}`}
+              onClick={() => handlePage(index)}>
+                {index + 1}
+              </button>
+            })
+          }
+          <buttton className="next-btn" onClick={nextPage}>
+            Next
+          </buttton>
+        </div>)
+      }
+      
     </section>
   </main>
 }
