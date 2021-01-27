f276fed953d8d5dd44ccad38a870aa8e761e555b
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-27 20:10:10 +0000

    React: Stock photo browser project: Boilerplate + API integration

--- courses/react/react-projects/19-stock-photos/setup/src/App.js
+++ courses/react/react-projects/19-stock-photos/setup/src/App.js
@@ -1,12 +1,58 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
-  return <h2>stock photos starter</h2>
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainUrl}${clientID}`;
+    
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setPhotos(data);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, [])
+  return <main>
+    <section className="search">
+      <form className="search-form">
+        <input type="text" placeholder="Search..." className="form-input"/>
+        <button type="submit" className="submit-btn" onClick={handleSubmit}>
+          <FaSearch />
+        </button>
+      </form>
+    </section>
+    <section className="photos">
+      <div className="photos-center">
+        {
+          photos.map((image, index) => {
+            return <Photo key={image.id} {...image} />
+          })
+        }
+      </div>
+      {loading && <h2 className="loading">Loading...</h2>}
+    </section>
+  </main>
 }
 
 export default App
