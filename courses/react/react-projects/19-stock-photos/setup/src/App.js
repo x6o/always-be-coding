129d56bc32c180299e668c11c8aaf2e3a419de47
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-27 20:35:06 +0000

    React: Stock photo browser project: implemented infinite scrolling

--- courses/react/react-projects/19-stock-photos/setup/src/App.js
+++ courses/react/react-projects/19-stock-photos/setup/src/App.js
@@ -9,16 +9,21 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
-    url = `${mainUrl}${clientID}`;
+    const urlPage = `&page=${page}`;
+    url = `${mainUrl}${clientID}${urlPage}`;
     
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setPhotos(data);
+      setPhotos((currentPhotos) => {
+        return [...currentPhotos, ...data]
+      });
       setLoading(false);
     } catch (error) {
       setLoading(false);
@@ -32,11 +37,26 @@ function App() {
 
   useEffect(() => {
     fetchImages();
-  }, [])
+  }, [page]);
+
+  useEffect(() => {
+    const event = window.addEventListener('scroll', () => {
+      if (!loading && (window.innerHeight + window.scrollY) >= document.body.scrollHeight - 200) {
+        setPage((currentValue) => {
+          return currentValue + 1
+        })
+      }
+    });
+    
+    return () => {
+      window.removeEventListener('scroll', event);
+    }
+  }, []);
+
   return <main>
     <section className="search">
       <form className="search-form">
-        <input type="text" placeholder="Search..." className="form-input"/>
+        <input type="text" placeholder="Beach, mountains, car..." className="form-input"/>
         <button type="submit" className="submit-btn" onClick={handleSubmit}>
           <FaSearch />
         </button>
