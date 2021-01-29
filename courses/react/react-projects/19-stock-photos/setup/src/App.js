10d9853860e44e7524ec0982a866a98056bd8aa4
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-29 22:15:18 +0000

    React: Stock photo browser project: implemented search functionality

--- courses/react/react-projects/19-stock-photos/setup/src/App.js
+++ courses/react/react-projects/19-stock-photos/setup/src/App.js
@@ -9,20 +9,35 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-  const [page, setPage] = useState(1);
-
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
 
   const fetchImages = async () => {
     setLoading(true);
     let url;
     const urlPage = `&page=${page}`;
-    url = `${mainUrl}${clientID}${urlPage}`;
+    const urlQuery = `&query=${query}`
     
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    }
+    else {
+      url = `${mainUrl}${clientID}${urlPage}`;
+    }
+
     try {
       const response = await fetch(url);
       const data = await response.json();
       setPhotos((currentPhotos) => {
-        return [...currentPhotos, ...data]
+        if (query && page === 1) {
+          return data.results
+        }
+        else if (query) {
+          return [...currentPhotos, ...data.results]
+        }
+        else {
+          return [...currentPhotos, ...data]
+        }
       });
       setLoading(false);
     } catch (error) {
@@ -33,6 +48,8 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setPage(1); // reset page value on seach
+    fetchImages();
   }
 
   useEffect(() => {
@@ -56,7 +73,8 @@ function App() {
   return <main>
     <section className="search">
       <form className="search-form">
-        <input type="text" placeholder="Beach, mountains, car..." className="form-input"/>
+        <input type="text" placeholder="Beach, mountains, car..." className="form-input"
+        value={query} onChange={(e) => setQuery(e.target.value) }/>
         <button type="submit" className="submit-btn" onClick={handleSubmit}>
           <FaSearch />
         </button>
@@ -65,8 +83,8 @@ function App() {
     <section className="photos">
       <div className="photos-center">
         {
-          photos.map((image, index) => {
-            return <Photo key={image.id} {...image} />
+          photos.map((image,index) => {
+            return <Photo key={index} {...image} />
           })
         }
       </div>
