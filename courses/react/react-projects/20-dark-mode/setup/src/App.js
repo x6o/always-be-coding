99ea122de00a7e918d658a2b0b6e3506377a96c9
Andrei Talpa <talpa.andrei@gmail.com>
2021-01-30 19:17:39 +0000

    React: Dark mode project: Moment.js & Toggle button

--- courses/react/react-projects/20-dark-mode/setup/src/App.js
+++ courses/react/react-projects/20-dark-mode/setup/src/App.js
@@ -3,11 +3,25 @@ import data from './data'
 import Article from './Article'
 
 function App() {
+  const [theme, setTheme] = useState('light-theme');
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  }
+
+  useEffect(() => {
+    document.documentElement.className = theme;  
+  }, [theme]);
+
   return <main>
     <nav>
       <div className="nav-center">
         <h1>dark mode</h1>
-        <button className="btn">toggle</button>
+        <button className="btn" onClick={toggleTheme}>toggle</button>
       </div>
     </nav>
     <section className="articles">
