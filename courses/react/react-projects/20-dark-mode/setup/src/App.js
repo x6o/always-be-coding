fd51e439307cda549b6dcc8542dbb9fd92f88604
Andrei Talpa <talpa.andrei@gmail.com>
2021-02-01 21:46:06 +0000

    React: Movie database project: Boilerplate code + initial API fetch

--- courses/react/react-projects/20-dark-mode/setup/src/App.js
+++ courses/react/react-projects/20-dark-mode/setup/src/App.js
@@ -2,8 +2,15 @@ import React, { useState, useEffect } from 'react'
 import data from './data'
 import Article from './Article'
 
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme
+}
 function App() {
-  const [theme, setTheme] = useState('light-theme');
+  const [theme, setTheme] = useState(getStorageTheme());
 
   const toggleTheme = () => {
     if (theme === 'light-theme') {
@@ -14,7 +21,8 @@ function App() {
   }
 
   useEffect(() => {
-    document.documentElement.className = theme;  
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
   }, [theme]);
 
   return <main>
--- courses/react/react-projects/21-movie-db/setup/src/context.js
+++ courses/react/react-projects/21-movie-db/setup/src/context.js
@@ -1,11 +1,41 @@
 import React, { useState, useContext, useEffect } from 'react'
 // make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=`
+export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState({ show:false, msg:'' });
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState('batman');
+  
+  const fetchMovies = async (url) => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if(data.Response === 'True') {
+        setMovies(data.Search);
+        setError({show:false, msg:''});
+      } else {
+        setError({show:true, msg: data.Error});
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [])
+  return <AppContext.Provider value={{
+    loading,
+    error,
+    movies,
+    query,
+    setQuery
+  }}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
