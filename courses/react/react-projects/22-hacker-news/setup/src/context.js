36f02d913b6f10a4f40f553635dab7578a342bfa
Andrei Talpa <talpa.andrei@gmail.com>
2021-02-04 22:01:16 +0000

    React: HN Portal Project: Fetch stories

--- courses/react/react-projects/22-hacker-news/setup/src/context.js
+++ courses/react/react-projects/22-hacker-news/setup/src/context.js
@@ -4,10 +4,14 @@ import * as reducerActions from './actions'
 
 import reducer from './reducer'
 
-const APIENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
 
 const initialState = {
-  isLoading: true
+  isLoading: true,
+  hits: [],
+  query: 'react',
+  page: 0,
+  nbPages: 0
 }
 
 const AppContext = React.createContext()
@@ -16,14 +20,28 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const fetchStories = async (url) => {
-    dispatch({type: reducerActions.SET_LOADING});
+    dispatch({ type: reducerActions.SET_LOADING });
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      dispatch({
+        type: reducerActions.SET_STORIES,
+        payload: {
+          hits: data.hits,
+          nbPages: data.nbPages
+        }
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   useEffect(() => {
-    fetchStories();
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
   }, [])
 
-  return <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>
+  return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
--- courses/react/react-projects/22-hacker-news/setup/src/reducer.js
+++ courses/react/react-projects/22-hacker-news/setup/src/reducer.js
@@ -4,6 +4,9 @@ const reducer = (state, action) => {
   switch(action.type) {
     case reducerActions.SET_LOADING:
       return {...state, isLoading: true}
+    case reducerActions.SET_STORIES:
+      const payload = action.payload;
+      return {...state, isLoading: false, hits: payload.hits, nbPages: payload.nbPages}
     default:
       throw new Error(`No action matched for "${action.type}" action type.`)
   }
