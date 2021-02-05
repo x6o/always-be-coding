cc28fbf3c35e506d5c91a1d57dde07d7264eb131
Andrei Talpa <talpa.andrei@gmail.com>
2021-02-05 15:50:28 +0000

    React: Redux Cart Project: Added Redux boilerplate

--- courses/react/react-redux/src/App.js
+++ courses/react/react-redux/src/App.js
@@ -1,17 +1,36 @@
 import React from "react";
+
 // components
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
+
 // items
 import cartItems from "./cart-items";
+
 // redux stuff
+import {createStore} from 'redux'
+
+// initial store
+const initialStore = {
+  count: 0
+};
+
+// reducer - fn to update store
+const reducer = (state, action) => {
+  console.log({state, action});
+  return state;
+};
+
+// store - aka state
+const store = createStore(reducer, initialStore);
+
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
